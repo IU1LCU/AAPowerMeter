b77a8e36e7704abff4ea8739b4d6e6c2ba358945
IU1LCU <165390094+IU1LCU@users.noreply.github.com>
2024-04-12 12:54:34 +0200

    Add files via upload

---
+++ powermonitor.ino
@@ -0,0 +1,163 @@
+/*
+==============================================================================================================
+A&A Electronics 2024 by IU1LCU & IU8HPE
+Voltage / Current remote monitor with 4 controlled relay
+Wiring for Ethernet (w5500): D13>SCLK / D12>MISO / D11>MOSI / D10>SCS
+Voltage readings A0>1 A1>2
+Current readings A2>1 A3>2 A4>3 A5>4
+Based on current sensor ACS712 for 5AMPS 185mv/1A
+Relay pins works as LOW = ON / HIGH = OFF
+For arduino relay modules
+==============================================================================================================
+*/
+#include <SPI.h>
+#include <Ethernet.h>
+int maxvolt = 5; //The maximun voltage of the voltage divider
+
+// Enter a MAC address and IP address for your controller below.
+// The IP address will be dependent on your local network:
+byte mac[] = { 
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+IPAddress ip(192, 168, 10, 117);
+
+// Initialize the Ethernet server library
+// with the IP address and port you want to use 
+// (port 80 is default for HTTP):
+EthernetServer server(80);
+
+// Relay states and pins
+String relayStates[] = {"Off", "Off", "Off", "Off"};
+const int relayPins[] = {2, 3, 4, 5};
+
+// Analog pins for voltage and current sensors
+const int voltagePins[] = {A0, A1}; // Analog pins for voltage sensors
+const int currentPins[] = {A2, A3, A4, A5}; // Analog pins for current sensors
+
+// Client variables 
+char linebuf[80];
+int charcount=0;
+
+void setup() { 
+  // Set relay pins as outputs
+  for (int i = 0; i < 4; i++) {
+    pinMode(relayPins[i], OUTPUT);
+    digitalWrite(relayPins[i], HIGH);
+  }
+  
+  // Open serial communication at a baud rate of 9600
+  Serial.begin(9600);
+  
+  // Start the Ethernet connection and the server:
+  Ethernet.begin(mac, ip);
+  server.begin();
+  Serial.print("Server is at ");
+  Serial.println(Ethernet.localIP());
+}
+
+// Display dashboard page with buttons to control relays
+void dashboardPage(EthernetClient &client) {
+
+ client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+          client.println();
+
+  client.println("<!DOCTYPE HTML><html><head>");
+  client.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+  client.println("<style>h3 {font-size: 22px;} h4 {font-size: 16px;}</style>");
+  client.println("</head><body>");       
+  client.println("<h3>A&A PowerMeter 2024</h3>");                                                      
+  client.println("<h3>Relay control</h3>");
+  // Generate buttons to control the relays
+  for (int i = 0; i < 4; i++) {
+    client.print("<h4>Relay ");
+    client.print(i + 1);
+    client.print(" - State: ");
+    client.print(relayStates[i]);
+    client.print(" </h4><a href=\"/relay");
+    client.print(i + 1);
+    client.print("on\"><button>ON</button></a><a href=\"/relay");
+    client.print(i + 1);
+    client.print("off\"><button>OFF</button></a><br>");
+  }
+  client.println("<h3>Analog readings</h3>");
+  // Print analog readings for voltage sensors
+  client.println("<ul>");
+  for (int i = 0; i < 2; i++) {
+    int sensorValue = analogRead(voltagePins[i]);
+    float voltage = (float(sensorValue) / 1023) * maxvolt;
+    client.print("<li>Voltage sensor ");
+    client.print(i + 1);
+    client.print(": ");
+    client.print(voltage);
+    client.println("V</li>");
+  }
+  client.println("</ul>");
+  // Print analog readings for current sensors
+  client.println("<ul>");
+  for (int i = 0; i < 4; i++) {
+    int sensorValue = analogRead(currentPins[i]);
+    int current_mA = (sensorValue / 1023.0) * 5000 / 185; // Conversione per ACS712 (185mV/A)
+    client.print("<li>Current sensor ");
+    client.print(i + 1);
+    client.print(": ");
+    client.print(current_mA);
+    client.println("mA</li>");
+  }
+  client.println("</ul>");
+  client.println("</body></html>"); 
+}
+
+void loop() {
+  // Listen for incoming clients
+  EthernetClient client = server.available();
+  if (client) {
+    Serial.println("New client");
+    memset(linebuf, 0, sizeof(linebuf));
+    charcount = 0;
+    // An HTTP request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        // Read char by char HTTP request
+        linebuf[charcount] = c;
+        if (charcount < sizeof(linebuf) - 1) charcount++;
+        // If you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the HTTP request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          dashboardPage(client);
+          break;
+        }
+        if (c == '\n') {
+          // Check which relay control URL was requested
+          for (int i = 0; i < 4; i++) {
+            if (strstr(linebuf, ("GET /relay" + String(i + 1) + "off").c_str()) > 0) {
+              digitalWrite(relayPins[i], HIGH);
+              relayStates[i] = "Off";
+            }
+            else if (strstr(linebuf, ("GET /relay" + String(i + 1) + "on").c_str()) > 0) {
+              digitalWrite(relayPins[i], LOW);
+              relayStates[i] = "On";
+            }
+          }
+          // You're starting a new line
+          currentLineIsBlank = true;
+          memset(linebuf, 0, sizeof(linebuf));
+          charcount = 0;          
+        } 
+        else if (c != '\r') {
+          // You've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // Give the web browser time to receive the data
+    delay(1);
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected");
+  }
+}
